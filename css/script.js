66293f7670a8e3044352112be58caf79304c7c09
zajictomas <90102632+zajictomas@users.noreply.github.com>
2021-09-05 01:08:39 +0200

    Add files via upload

--- css/script.js
+++ css/script.js
@@ -31,5 +31,25 @@
           $("html, body").animate({scrollTop: $(".jq--photo-gallery").offset().top}, 2500); 
        });
        
+        /* Mobile navigation */
+      $(".jq--nav-icon").click(function(){
+         $(".nav-background").slideToggle();
+         $(".mobile-nav-back").fadeToggle();
+         $("nav ul").fadeToggle();
+      }); 
+       
+        /* Change hamburger to cross vice versa */
+      $(".jq--image-hamburger").click(function(){
+          
+          if($(".jq--image-hamburger").attr("src") == "images/hamburgerMenu.png")
+          {
+              $($(".jq--image-hamburger").attr("src","images/crossMenu.png"));
+          }
+          else
+          {
+              $($(".jq--image-hamburger").attr("src","images/hamburgerMenu.png"));
+          }
+      });
+       
    }); 
 })(jQuery);
